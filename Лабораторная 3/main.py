be378ba3bbf0c0faaebc75f78acb839c54a86730
sbryut <118308801+sbryut@users.noreply.github.com>
2024-01-29 20:39:15 +0100

    Add lab-3

--- Лабораторная 3/main.py
+++ Лабораторная 3/main.py
@@ -1,8 +1,16 @@
 class Book:
     """ Базовый класс книги. """
     def __init__(self, name: str, author: str):
-        self.name = name
-        self.author = author
+        self._name = name
+        self._author = author
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def author(self):
+        return self._author
 
     def __str__(self):
         return f"Книга {self.name}. Автор {self.author}"
@@ -11,21 +19,45 @@ class Book:
         return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
 
 
-class PaperBook:
+class PaperBook(Book):
     def __init__(self, name: str, author: str, pages: int):
-        self.name = name
-        self.author = author
-        self.pages = pages
+        super().__init__(name, author)
+        self._pages = pages
+
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, value):
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError("Количество страниц должно быть положительным целым числом.")
+        self._pages = value
 
     def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+        return f"{super().__str__()} Количество страниц: {self.pages}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages})"
 
 
-class AudioBook:
+class AudioBook(Book):
     def __init__(self, name: str, author: str, duration: float):
-        self.name = name
-        self.author = author
-        self.duration = duration
+        super().__init__(name, author)
+        self._duration = duration
+
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def duration(self, value):
+        if not isinstance(value, (int, float)) or value <= 0:
+            raise ValueError("Продолжительность должна быть положительным числом.")
+        self._duration = value
 
     def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+        return f"{super().__str__()} Продолжительность: {self.duration} часов"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration})"
